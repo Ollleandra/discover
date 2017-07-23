557600089d24709c2065e4689ed9f41a3c99b241
oleandra <ollleandra@gmail.com>
2017-07-23 10:15:07 +0300

    edit mobile + tel

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ $(document).ready(function(){
         $(this).toggleClass("is-active");
     });
 });
-
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCl7XHJcbiAgICAkKFwiLmhhbWJ1cmdlclwiKS5jbGljayhmdW5jdGlvbigpe1xyXG4gICAgICAgICQodGhpcykudG9nZ2xlQ2xhc3MoXCJpcy1hY3RpdmVcIik7XHJcbiAgICB9KTtcclxufSk7Il0sImZpbGUiOiJtYWluLmpzIn0=
